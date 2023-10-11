5b83351d980f6d6eb959430a5a81019e3f5b59fd
markw11 <mark.wang1@gmail.com>
2023-10-11 09:49:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,6 +12,18 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+function git_sparse_clone() {
+    git clone --filter=blob:none --no-checkout --depth=1 $1 $2 && cd $2
+    git sparse-checkout init --cone
+    git sparse-checkout set $3
+    git checkout
+    mv $3 ../$4
+    cd ../ && rm -rf $2
+}
+
+# Add mtkiappd support for 802.11 k/v/r
+git_sparse_clone https://github.com/coolsnowwolf/lede lede package/lean/mt/mtkiappd package/kernel/mt-drivers/mtkiappd
+
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages.git' >>feeds.conf.default
